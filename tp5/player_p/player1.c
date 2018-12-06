f4dcc69b60f896c93a2a553823d62f08d031f83a
Clement Guillemette <cleguillem37@yahoo.fr>
2018-12-06 13:40:36 +0100

    tp5

---
+++ tp5/player_p/player1.c
@@ -0,0 +1,19 @@
+#include "actions.h"
+#include <stdio.h>
+#define MAX_PLAY_ACTION 20
+
+char play_actions[] = {
+    ACTION_TELEPORT_D,    
+};
+
+char get_action()
+{
+    char ret_val = ACTION_TELEPORT_D;
+
+    return (ret_val);
+}
+
+void player_init(t_player* p_player, uint8_t num)
+{
+    
+}
